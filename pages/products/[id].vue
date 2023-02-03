c53a3f2d5aa974b1b081a9e3b091e30af27ced7e
Sophie Kowalski <sophie@owlie.xyz>
2023-02-03 15:56:48 +0100

    Adding basic route with dynamic parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti fuga
+      assumenda nam consequatur reiciendis officia tempora nemo voluptatem
+      praesentium ipsa, sit reprehenderit libero deserunt! Minus odio non
+      voluptatibus quia consectetur.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped></style>
