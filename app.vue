51b6f7196db942901e19e1ac00986bb3a0d15404
Sophie Kowalski <sophie@owlie.xyz>
2023-02-03 15:42:20 +0100

    Adding basic static pages

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <p>Hello you</p>
-  </div>
-</template>
---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente saepe
+      blanditiis dolore reiciendis, aliquam officiis ad totam neque recusandae
+      aspernatur voluptates ex dignissimos pariatur cum, omnis praesentium velit
+      ullam possimus.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+      dignissimos! Vero voluptas nostrum corrupti, assumenda culpa quidem
+      voluptatibus dolorum, possimus cum doloribus nisi ad accusamus blanditiis
+      animi molestias at aliquid!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
