d70c4459af796385f8a1ce52ea9d4a812bff5a5a
Sophie Kowalski <sophie@owlie.xyz>
2023-02-07 09:26:31 +0100

    Update uri api/currency/[code]

--- server/api/currency/[code].js
+++ server/api/currency/[code].js
@@ -2,7 +2,8 @@ export default defineEventHandler(async (event) => {
   const { code } = event.context.params
   const { currencyKey } = useRuntimeConfig()
 
-  const uri = `https://api.currencyapi.com/v3/currencies?apikey=${currencyKey}&currencies=${code}`
+  // const uri = `https://api.currencyapi.com/v3/currencies?apikey=${currencyKey}&currencies=${code}`
+  const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`
 
   const { data } = await $fetch(uri)
 
